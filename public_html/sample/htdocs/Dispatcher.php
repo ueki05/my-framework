8077108f746dc5610ca60f70eb9e78d49dfb5eb1
ueki05 <uekiyuta1991@gmail.com>
2018-04-06 23:22:04 +0900

    コントローラーの振り分け

--- public_html/sample/htdocs/Dispatcher.php
+++ public_html/sample/htdocs/Dispatcher.php
@@ -1,13 +1,15 @@
 <?php
 
-class Dispatcher
+abstract class Dispatcher
 {
   public function dispatch()
   {
+    // パラメーター取得（末尾の / は削除）
+    $param = ereg_replace('/?$', '', $_GET['param']);
     $params = array();
-    if ('' != $_SERVER['REQUEST_URI']) {
-      // パラメーターを"/"で分割
-      $params = explode('/', $_SERVER['REQUEST_URI']);
+    if ('' != $param) {
+      // パラメーターを / で分割
+      $params = explode('/', $param);
     }
 
     // １番目のパラメーターをコントローラーとして取得
@@ -17,22 +19,62 @@ class Dispatcher
     }
 
     // パラメータより取得したコントローラー名によりクラス振分け
+    $controllerInstance = $this->dispatchController($controller);
+      if (null == $controllerInstance) {
+        header("HTTP/1.0 404 Not Found");
+        exit;
+      }
+    // （以下略）
+  }
+
+  // 振分け処理を抽象化
+  abstract protected function dispatchController($controller);
+}
+
+// 通販サイト振分け処理クラス
+class TuuhanDispatcher extends Dispatcher
+{
+  // 振分け処理メソッドを実装
+  protected function dispatchController($controller)
+  {
     $controllerInstance = null;
     switch ($controller) {
-    case 'index':
-      $controllerInstance = new IndexController();
+    case 'list': 
+      $controllerInstance = new ListController();
       break;
-    case 'product':
-      $controllerInstance = new ProductController();
+    case 'detail': 
+      $controllerInstance = new DetailController();
       break;
-    case 'cart':
+    case 'cart': 
       $controllerInstance = new CartController();
       break;
-    default:
-      header("HTTP/1.0 404 Not Found");
-      exit;
+    case 'customer': 
+      $controllerInstance = new CustomerController();
       break;
     }
+    return $controllerInstance;
+  }
+}
+
+// お料理サイト振分け処理クラス
+class CockingDispatcher extends Dispatcher
+{
+  // 振分け処理メソッドを実装
+  protected function dispatchController($controller)
+  {
+    $controllerInstance = null;
+    switch ($controller) {
+    case 'recipe': 
+      $controllerInstance = new RecipeController();
+      break;
+    case 'bbs': 
+      $controllerInstance = new BbsController();
+      break;
+    case 'link': 
+      $controllerInstance = new LinkController();
+      break;
+    }
+    return $controllerInstance;
   }
 }
 
