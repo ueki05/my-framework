ec667018231ed8b56fafb4488983543082ae29d7
ueki05 <uekiyuta1991@gmail.com>
2018-04-08 02:09:24 +0900

    リクエスト変数クラス

---
+++ library/mvc/QueryString.php
@@ -0,0 +1,12 @@
+<?php
+
+// GET変数クラス
+class QueryString extends RequestVariables
+{
+  protected function setValues()
+  {
+    foreach ($_GET as $key => $value) {
+      $this->_values[$key] = $value;
+    }
+  }
+}
