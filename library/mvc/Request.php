ac65316ae4a019faaf9191c5ab31f3dc05856c22
ueki05 <uekiyuta1991@gmail.com>
2018-04-08 02:11:44 +0900

    リクエストクラス

---
+++ library/mvc/Request.php
@@ -0,0 +1,40 @@
+<?php
+
+class Request
+{
+  // POSTパラメータ
+  private $post;
+  // GETパラメータ
+  private $query;
+
+  // コンストラクタ@
+  public function __construct()
+  {
+    $this->post = new Post();
+    $this->query = new QueryString();
+  }
+
+  // POST変数取得
+  public function getPost($key = null)
+  {
+    if (null == $key) {
+      return $this->post->get();
+    }
+    if (false == $this->post->has($key)) {
+      return null;
+    }
+    return $this->post->get($key);
+  }
+
+  // GET変数取得
+  public function getQuery($key = null)
+  {
+    if (null == $key) {
+      return $this->query->get();
+    }
+    if (false == $this->query->has($key)) {
+      return null;
+    }
+    return $this->query->get($key);
+  }
+}
