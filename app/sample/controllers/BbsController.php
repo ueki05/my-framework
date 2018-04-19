c699df39c55503ecd6fcc20698867027e2b6fbb2
ueki05 <uekiyuta1991@gmail.com>
2018-04-19 23:45:30 +0900

    掲示板: コントローラー

--- app/sample/controllers/BbsController.php
+++ app/sample/controllers/BbsController.php
@@ -3,16 +3,44 @@
 class BbsController extends ControllerBase
 {
   // 記事表示
-  public function threadsAction()
+  public function indexAction()
   {
-    // URLパラメータよりページ数取得
-    $page = 1;
-    if (null != $this->request->getParam('page')) {
-      $page = $this->request->getParam('page');
-    }
-    // 記事データ取得
-    $data = $this->model->getThreadsData($page); // Undefined property: BbsController::$model
-    // 表示
-    $this->view->assign('parent', $data);
+    // データ取得
+    $bbs = new Bbs();
+    $threads = $bbs->getThreads(100);
+    // テンプレートへ出力
+    $this->view->assign('threads', $threads);
+  }
+
+  // 返信モードへ以降
+  public function resAction()
+  {
+    $post = $this->request->getPost();
+
+    $this->view->assign('parent_no', $post["parent_no"]);
+    $this->indexAction();
+    $this->templatePath = 'bbs/index.tpl';
+  }
+
+  // 投稿処理
+  public function registAction()
+  {
+    $post = $this->request->getPost();
+
+    $bbs = new Bbs();
+    $bbs->regist($post);
+
+    $this->redirect('/bbs/index');
+  }
+
+  // 削除処理
+  public function deleteAction()
+  {
+    $post = $this->request->getPost();
+
+    $bbs = new Bbs();
+    $bbs->del($post['no']);
+
+    $this->redirect('/bbs/index');
   }
 }
